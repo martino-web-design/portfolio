96b81c4e55add8ecf9864120c0847c34d8a154a9
martino-web-design <56980182+martino-web-design@users.noreply.github.com>
2019-10-29 21:45:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+ $(document).ready(function(){
+   //--- Smooth Scrolling to link section ---
+    var scrollLink = $('.smooth'); // css class add to link tags
+    scrollLink.click(function(e){
+      e.preventDefault();
+      $('body,html').animate({scrollTop: $(this.hash).offset().top-75}, 1000)
+     });
+
+  $(window).scroll(function(){
+    //--- Nav menu add box shadow on scroll ---
+    if ($(window).scrollTop() >= 30) {
+      $('header nav').css({'box-shadow': '0px 0px 10px #555'});
+    }
+    else {
+      $('header nav').css('box-shadow', '0 0 0 #fff');
+    }
+    //--- Scrolling Add Active Nav Class ---
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function(){
+      var sectionOffset= $(this.hash).offset().top -150;
+      if(sectionOffset <= scrollbarLocation){
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+      };
+    }); // end scrollLink func.
+  }); //end window scroll
+
+  // --- Menu collapse after target selected ---
+  const navUl = document.querySelector('#navbarNav ul');
+  const navB = document.querySelector('#navbarNav');
+
+  navUl.addEventListener('click',function(){
+    navB.classList.toggle('show');
+  });
+
+  // --- Home button onclick remove active nav class ---
+  $('#home').click(function(){
+   $('.nav-item').removeClass('active');
+  });
+  
+});//end of JQuery doc ready
