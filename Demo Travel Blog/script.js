1c06dce41364cd8ead9e85a7cb5774f014af65dc
martino-web-design <56980182+martino-web-design@users.noreply.github.com>
2019-11-06 21:54:34 -0800

    Add files via upload

--- Demo Travel Blog/script.js
+++ Demo Travel Blog/script.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
 
-  // Flash message function
+  // --- Flash message function ---
   setTimeout(function(){
     $('#flashMessage').slideDown(1500).delay(4000).slideUp(1500);
   },3000)
 
-  //--- Smooth Scrolling to link section ---
+  // --- Smooth Scrolling to link section ---
    var scrollLink = $('.nav-item'); // css class add to link tags
    scrollLink.click(function(e){
      e.preventDefault();
      $('body,html').animate({scrollTop: $(this.hash).offset().top-75}, 1000)
     });
- });
 
-// Add current date
-var date = new Date();
-document.querySelector('#date').textContent = date.toDateString();
+  // --- Toggle menu ---
+  const navB = document.querySelector('#navbarNavAltMarkup .navbar-nav');
+  const navDrop = document.querySelector('#navbarNavAltMarkup');
+  navB.addEventListener('click', function(){
+    if(window.innerWidth < 576){
+      navDrop.classList.toggle('show');
+    }
+  });
+
+  // --- Add current date ---
+  var date = new Date();
+  document.querySelector('#date').textContent = date.toDateString();
+
+}); // end doc ready
