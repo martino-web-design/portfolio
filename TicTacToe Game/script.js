23af45b3502ae03b56deb1a2b23ad99733168e52
martino-web-design <56980182+martino-web-design@users.noreply.github.com>
2019-11-10 21:57:58 -0800

    Add files via upload

--- TicTacToe Game/script.js
+++ TicTacToe Game/script.js
@@ -2,21 +2,19 @@ $(document).ready(function(){
 
 //  Start function resets board and selects player to begin
   function start(){
-
     $('#game').click(function(){
-
-      $('.modal-overlay').css('opacity','0');
+      $('.start-modal').css('display', 'none');
       $('.square').removeClass('X').removeClass('O');
 
-      // Player selection, randomly determines if X or O starts game
+      // Player selection, randomly determines if player X or O starts game
       var selection = Math.round(Math.random()+1);
       if (selection === 1){
-        $('.modal-overlay').css('opacity','1');
+        $('.modal-outer').css('display', 'block');
         $('.modal-wrap p').text("Player X gets to go first!");
         player = 1;
       }
       else {
-        $('.modal-overlay').css('opacity','1');
+        $('.modal-outer').css('display', 'block');
         $('.modal-wrap p').text("Player O gets to go first!");
         player = 2;
       }
@@ -50,70 +48,70 @@ $(document).ready(function(){
     else if ($('.3').hasClass(i) && $('.5').hasClass(i) && $('.7').hasClass(i)) {
       return true;
     }
-
   }
 
 // Tie function, determines if game is tied
   function tie(){
     if (count== 9){
-      $('.modal-overlay').css('opacity','1');
+      $('.modal-outer').css('display','block');
       $('.modal-wrap p').text("It's a tie, have another go!");
+      $('.start-modal').css('display', 'block');
     }
   }
-// ----- end of functions ------
 
 //  ----- Start Game -----
-alert("Decide who wants to be player X or player O to begin.");
 start();
+
 win_1 = 0;
 win_2 = 0;
 
+$('.close').click(function(){
+  $('.modal-outer').css('display', 'none');
+});
+
 // ----- game play begins with click function
-  $('.square').click(function(){
-    $('#x-start').hide();
-    $('.modal-overlay').css('opacity','0');
+$('.square').click(function(){
    if ($(this).hasClass('X') || $(this).hasClass('O') ){
-     $('.modal-overlay').css('opacity','1');
+     $('.modal-outer').css('display','block');
      $('.modal-wrap p').text("This spot is already taken, try again!");
     }
+   else if (player === 1){
+     $(this).addClass('X');
 
-   else {
-        if (player === 1) {
-          $(this).addClass('X');
-
-           if (winner('X')){
-             $('.modal-overlay').css('opacity','1');
-             $('.modal-wrap p').text("Player X is the winner!");
-             win_1+= 1;
-             $('#win_1').text("Player X has " + win_1 + " win!");
-             if(win_1>1){
-               $('#win_1').text("Player X has " + win_1 + " wins!");
-             }
+        if (winner('X')){
+            $('.modal-outer').css('display','block');
+            $('.modal-wrap p').text("Player X is the winner!");
+            win_1+= 1;
+            $('#win_1').text("Player X has " + win_1 + " win!");
+            if(win_1>1){
+              $('#win_1').text("Player X has " + win_1 + " wins!");
             }
-            else {
-              count+= 1;
-              tie()
-              player = 2;
+            $('.start-modal').css('display', 'block');
             }
-        }
         else {
-          $(this).addClass('O');
+            count+= 1;
+            tie()
+            player = 2;
+            }
+    }
+    else if (player === 2){
+      $(this).addClass('O');
 
-          if (winner('O')){
-            $('.modal-overlay').css('opacity','1');
+        if (winner('O')){
+            $('.modal-outer').css('display','block');
             $('.modal-wrap p').text("Player O is the winner!");
             win_2+= 1;
             $('#win_2').text("Player O has " + win_2 + " win!");
             if(win_2>1){
               $('#win_2').text("Player O has " + win_2 + " wins!");
             }
+            $('.start-modal').css('display', 'block');
+            }
+        else {
+           count+= 1;
+           tie()
+           player = 1;
            }
-           else {
-             count+= 1;
-             tie()
-             player = 1;
-           }
-        }
     }
   }); // ----- end square click function
 }); // ----- end document ready function
