021e60dccc4e4b6dbdc06620b9efb23a39074b31
martino-web-design <56980182+martino-web-design@users.noreply.github.com>
2019-12-12 16:08:25 -0800

    Add files via upload

--- Calculator App/script.js
+++ Calculator App/script.js
@@ -1,66 +1,118 @@
+const display = document.querySelector('#display');
 
-
-
+const inputOne = document.querySelector('#input1');
+const inputTwo = document.querySelector('#input2');
+const operator = document.querySelector('#operator');
+const totalCount = document.querySelector('#total');
 // ----  Calc Function ----
 
 function calc(){
-  var one = parseFloat($('#input1').text());
-  var two = parseFloat($('#input2').text());
-  var oper = $('#operator').text();
-  var total;
+   one = parseFloat(inputOne.textContent);
+   two = parseFloat(inputTwo.textContent);
+   oper = operator.textContent;
+   totalAmount= "";
 
   if (oper == "+"){
-    total = one + two;
+    totalAmount = one + two;
   }
   else if (oper == "-"){
-    total = one - two;
+    totalAmount = one - two;
   }
   else if (oper == "/"){
-    total = one / two;
+    totalAmount = one / two;
   }
   else if (oper == "*"){
-    total = one * two;
+    totalAmount = one * two;
   }
-  total = total.toLocaleString();
-  $('#input2').hide();
-  $('#total').text(total).show();
 
 }
 
 // --- Number buttons ---
 $('button.num').click(function(){
-   if ($('#operator').html()!=''){
-     $('#operator').hide();
-     $('#input2').show();
-     $('#input2').append(this.id).innerText();
-   }
-   else if(this){
-     $('#input1').append(this.id).innerText();
-   }
+  colorRemove(this);
+
+  if(inputOne.textContent != "" && operator.textContent != "" ){
+
+    number = this.textContent;
+    $('#input2').append(number);
+    calc();
+    totalCount.textContent = totalAmount;
+    display.textContent = inputTwo.textContent;
+
+    console.log("tot", totalAmount)
+    }
+
+  else {
+    number = this.textContent;
+    $('#input1').append(number);
+    $('#display').append(number);
+
+    console.log("else",inputOne.textContent)
+    console.log("n",number)
+  }
 });
 
 // --- Operator buttons ---
 $('button.oper').click(function(){
-   if(this){
-      var clicked = this.innerHTML;
-      $('#operator').html(clicked);
-      $('#input1').hide();
-      $('#operator').show();
+
+  let e = event.target;
+  e.classList.add('color');
+  setTimeout(function(){
+    e.classList.remove('color');
+  },200);
+
+  if (operator.textContent == "" && inputTwo.textContent == ""){
+    operator.textContent = this.textContent;
+    display.textContent = inputOne.textContent;
+
+    console.log("first oper")
+    }
+
+  else if (inputOne.textContent != "" && inputTwo.textContent != "" && operator.textContent != ""){
+
+    operator.textContent = this.textContent;
+    display.textContent = totalAmount.toLocaleString();
+    inputOne.textContent = totalCount.textContent;
+    inputTwo.textContent = "";
+
+    console.log("additional opers")
   }
 });
 
 // --- Equal button ---
 $('button.equal').click(function(){
-  if(this){
-    calc();
-  }
+
+     calc();
+     console.log(totalAmount);
+     totalCount.textContent = totalAmount;
+     display.textContent = totalAmount.toLocaleString();
+
+
  });
 
 // ---- Clear button ----
+
 $('button#clear').click(function(){
   $('#input1').text("");
-  $('#input2').text("").hide();
-  $('#operator').text("").hide();
-  $('#total').text("").hide();
-  $('#input1').show();
+  $('#input2').text("");
+  $('#operator').text("");
+  $('#total').text("");
+  $('#display').text("");
+
 });
+
+// ---- Button Effects ---
+/*
+$('button.oper').mousedown(function(){
+
+this.style.backgroundColor = "orange";
+
+});
+*/
+function btnColor(x){
+ // x.style.backgroundColor = "orange";
+  // setTimeout(btnColor, 500);
+}
+function colorRemove(x){
+ // x.style.backgroundColor = "#666";
+}
